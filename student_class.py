12288d9752cb5115ef019875ed1e1a47b6bb1bf2
curiouscoder25 <gauravkathayat12@gmail.com>
2025-05-27 15:23:44 +0545

    viechle.py and student.py

---
+++ student_class.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__ (self,name,roll_no,marks):
+        self.name=name
+        self.roll_no=roll_no
+        self.marks=marks
+    def display_student_details(self):
+        print(f"Name: {self.name} Roll.no: {self.roll_no} Average Marks: {self.average}")
+    
+    def average(self):
+        return sum(self.marks)/len(self.marks)
+        
+gaurav=student("Gaurav",7,[99,99,89,98,78])
+gaurav.display_student_details()
---
+++ veichle.py
@@ -0,0 +1,16 @@
+#wrong xa 
+class veichle:
+    def __init__ (self,make,model):
+        self.make=make
+        self.model=model
+
+class Car(veichle):
+    def __init__(self,milage,capacity,make,model):
+        self.milage=milage
+        self.capacity=capacity
+
+class display(self):
+    print(f"Milage:{milage} Capacity")
+
+car=Car("40","10",2027,L973)
+car.display()
